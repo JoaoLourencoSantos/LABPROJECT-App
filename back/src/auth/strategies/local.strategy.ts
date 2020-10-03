d316198f44087bbba625db41e0e3ed5b8b1a2c5e
João marcos <lourencosjm41@gmail.com>
2020-10-02 21:59:04 -0300

    fix

--- back/src/auth/strategies/local.strategy.ts
+++ back/src/auth/strategies/local.strategy.ts
@@ -1,8 +1,8 @@
 import { AuthService } from './../auth.service';
-import { User } from 'firebase';
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import User from 'src/app/models/user';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
